fa2d41e34eace8ba390b3b638cd758cb4129abbc
Billy Charlton <billy@okbecause.com>
2021-01-27 20:50:57 +0100

    add latest updates button

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -64,10 +64,8 @@ class HomeSplash extends React.Component {
         <div className="inner">
           <ProjectTitle tagline={siteConfig.tagline} title={siteConfig.title} />
           <PromoSection>
-            <Button href={docUrl("aftersim-intro")}>Get Started</Button>
-            {/* <Button href="#try">Try It Out</Button>
-            <Button href={docUrl("doc1.html")}>Example Link</Button>
-            <Button href={docUrl("doc2.html")}>Example Link 2</Button> */}
+            <Button href={docUrl("aftersim-intro")}>Go to the Docs</Button>
+            <Button href={`${baseUrl}blog`}>Latest Updates</Button>
           </PromoSection>
         </div>
       </SplashContainer>
